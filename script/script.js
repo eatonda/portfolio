ab078c9f889f00b943d2ca3016fa851ef4f6d5b9
David Eaton <eatonda@oregonstate.edu>
2020-11-25 22:58:19 -0700

    Fixed resume.

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	var typed = new Typed(".typed", {
+		strings: ["Hello World!"],
+		typeSpeed: 80,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+});
